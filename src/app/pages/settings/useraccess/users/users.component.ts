28f0f883bdf4894fe3a83cbf6195ca355ef24b08
NimanthaH <m2sandeepa@gmail.com>
2024-05-01 23:50:50 +0530

    disaster

--- src/app/pages/settings/useraccess/users/users.component.ts
+++ src/app/pages/settings/useraccess/users/users.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { LayoutBodyComponent } from 'protrack/components';
+import { LayoutComponent } from 'protrack/components';
+import { LayoutHeaderComponent } from 'protrack/components';
+import { LayoutSidebarComponent } from 'protrack/components';
+import { LayoutFooterComponent } from 'protrack/components';
+import { LayoutTopbarComponent } from 'protrack/components';
+import { MatIcon } from '@angular/material/icon';
+import { TrutAnnouncementModule } from 'protrack/components';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [
+    LayoutBodyComponent,
+    LayoutComponent,
+    LayoutHeaderComponent,
+    LayoutSidebarComponent,
+    LayoutFooterComponent,
+    LayoutTopbarComponent,
+    MatIcon,
+    TrutAnnouncementModule
+  ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
